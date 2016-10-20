3afaa7ca251362fb85c1524182050c5d59d97339
elliotjb <elliot.jimenez1@gmail.com>
2016-10-20 13:13:55 +0200

    Correct Chains Map and Blit of Ball

--- ModuleSceneIntro.cpp
+++ ModuleSceneIntro.cpp
@@ -25,7 +25,7 @@ bool ModuleSceneIntro::Start()
 
 	App->renderer->camera.x = App->renderer->camera.y = 0;
 
-	circle = App->textures->Load("pinball/wheel.png"); 
+	circle = App->textures->Load("pinball/Ball.png"); 
 	box = App->textures->Load("pinball/crate.png");
 	rick = App->textures->Load("pinball/Map_Pinball.png");
 	bonus_fx = App->audio->LoadFx("pinball/bonus.wav");
@@ -90,14 +90,24 @@ update_status ModuleSceneIntro::Update()
 	fVector normal(0.0f, 0.0f);
 
 	// All draw functions ------------------------------------------------------
-	p2List_item<PhysBody*>* c = circles.getFirst();
+	p2List_item<PhysBody*>* c = ricks.getFirst();
+
+
+	while (c != NULL)
+	{
+		int x, y;
+		c->data->GetPosition(x, y);
+		App->renderer->Blit(rick, x, y, NULL, 1.0f, c->data->GetRotation());
+		c = c->next;
+	}
+
+	c = circles.getFirst();
 
 	while(c != NULL)
 	{
 		int x, y;
 		c->data->GetPosition(x, y);
-		if(c->data->Contains(App->input->GetMouseX(), App->input->GetMouseY()))
-			App->renderer->Blit(circle, x, y, NULL, 1.0f, c->data->GetRotation());
+		App->renderer->Blit(circle, x - 4.7f, y-3.0f, NULL, 1.0f);
 		c = c->next;
 	}
 
@@ -117,15 +127,7 @@ update_status ModuleSceneIntro::Update()
 		c = c->next;
 	}
 
-	c = ricks.getFirst();
 
-	while(c != NULL)
-	{
-		int x, y;
-		c->data->GetPosition(x, y);
-		App->renderer->Blit(rick, x, y, NULL, 1.0f, c->data->GetRotation());
-		c = c->next;
-	}
 
 	// ray -----------------
 	if(ray_on == true)
@@ -166,51 +168,54 @@ void ModuleSceneIntro::OnCollision(PhysBody* bodyA, PhysBody* bodyB)
 
 bool ModuleSceneIntro::CreateMap()
 {
-	int rick_head[88] = {
-		216, 660,
-		41, 558,
-		90, 369,
-		136, 370,
-		161, 357,
-		130, 257,
+	int rick_head[94] = {
+		215, 662,
+		40, 562,
+		93, 371,
+		120, 269,
+		128, 260,
 		122, 248,
-		125, 232,
-		130, 221,
-		144, 219,
-		152, 223,
+		124, 235,
+		130, 224,
+		143, 222,
+		154, 229,
 		164, 250,
 		170, 249,
-		147, 179,
+		144, 182,
 		177, 65,
-		187, 51,
-		204, 41,
-		222, 33,
-		240, 29,
-		256, 27,
-		403, 26,
-		419, 28,
-		434, 32,
-		445, 35,
-		459, 40,
+		187, 50,
+		205, 37,
+		224, 29,
+		242, 26,
+		260, 25,
+		406, 25,
+		422, 27,
+		436, 30,
+		449, 34,
+		460, 40,
 		470, 47,
 		478, 54,
 		486, 64,
 		490, 73,
-		650, 657,
-		655, 672,
-		650, 686,
-		635, 686,
-		628, 678,
+		650, 653,
+		648, 662,
+		638, 668,
+		625, 662,
+		621, 654,
 		594, 565,
 		582, 552,
 		511, 255,
-		502, 253,
-		497, 264,
-		464, 354,
-		492, 370,
-		530, 371,
-		577, 554,
-		394, 660
+		504, 255,
+		513, 270,
+		534, 370,
+		578, 564,
+		400, 662,
+		735, 705,
+		722, -40,
+		-37, -50,
+		-25, 715,
+		215, 720,
+		214, 682
 	};
 
 	int PartUP_right[40] = {
@@ -236,88 +241,109 @@ bool ModuleSceneIntro::CreateMap()
 		453, 251
 	};
 
-	int PartUP_center[14] = {
-		352, 49,
-		352, 60,
-		351, 67,
-		344, 67,
-		344, 56,
-		345, 46,
-		350, 46
+	int PartUP_center[22] = {
+		348, 67,
+		345, 67,
+		343, 65,
+		343, 58,
+		343, 52,
+		345, 49,
+		348, 48,
+		351, 49,
+		352, 52,
+		352, 62,
+		351, 66
 	};
 
-	int PartUP_center_2[16] = {
-		319, 48,
-		320, 54,
-		319, 60,
+	int PartUP_center_2[24] = {
+		320, 53,
+		319, 51,
+		317, 50,
+		315, 50,
+		313, 51,
+		312, 53,
+		312, 61,
+		313, 63,
 		316, 64,
-		312, 60,
-		312, 50,
-		313, 47,
-		316, 45
+		318, 64,
+		320, 63,
+		320, 57
 	};
-	int PartUp_Left[42] = {
-		288, 46,
-		287, 63,
-		256, 72,
-		246, 77,
-		240, 136,
-		230, 134,
+
+	int PartUp_Left[56] = {
+		228, 125,
 		236, 69,
 		209, 68,
 		206, 133,
 		200, 136,
-		198, 147,
-		193, 148,
-		190, 141,
-		203, 78,
-		207, 62,
-		215, 52,
-		230, 45,
+		196, 145,
+		190, 145,
+		189, 139,
+		202, 77,
+		206, 65,
+		214, 55,
+		227, 46,
 		242, 42,
-		261, 41,
-		276, 40,
-		287, 41
+		258, 40,
+		278, 40,
+		287, 40,
+		287, 46,
+		287, 57,
+		284, 60,
+		256, 67,
+		249, 71,
+		241, 129,
+		239, 133,
+		234, 132,
+		232, 129,
+		229, 133,
+		226, 132,
+		227, 129
 	};
 
-	int Part_Center_1[74] = {
-		363, 195,
-		366, 206,
-		367, 251,
+	int Part_Center_1[84] = {
+		366, 218,
+		365, 190,
+		363, 183,
+		359, 179,
+		354, 174,
+		348, 171,
+		343, 169,
+		337, 167,
+		331, 165,
+		323, 164,
+		310, 163,
+		302, 164,
+		293, 166,
+		287, 168,
+		281, 171,
+		276, 174,
+		271, 179,
+		268, 184,
+		266, 211,
+		264, 245,
+		265, 252,
+		268, 254,
+		273, 253,
+		275, 246,
+		276, 203,
+		280, 191,
+		287, 183,
+		298, 178,
+		312, 176,
+		328, 177,
+		337, 180,
+		345, 184,
+		351, 189,
+		354, 195,
+		355, 203,
+		355, 226,
+		355, 247,
 		356, 254,
-		356, 208,
-		351, 193,
-		347, 186,
-		338, 182,
-		328, 179,
-		317, 179,
-		310, 179,
-		302, 180,
-		297, 182,
-		290, 185,
-		285, 191,
-		282, 196,
-		278, 203,
-		277, 213,
-		276, 224,
-		276, 232,
-		276, 242,
-		274, 254,
-		265, 254,
-		268, 202,
-		272, 190,
-		277, 183,
-		287, 177,
-		296, 174,
-		305, 172,
-		314, 171,
-		319, 171,
-		328, 172,
-		337, 174,
-		342, 176,
-		351, 180,
-		356, 184,
-		360, 189
+		361, 256,
+		366, 253,
+		366, 243,
+		366, 224
 	};
 
 	int Part_Center_2[22] = {
@@ -379,55 +405,103 @@ bool ModuleSceneIntro::CreateMap()
 		501, 401
 	};
 
-	int Boton_Left[16] = {
-		288, 106,
-		281, 107,
-		278, 112,
-		281, 116,
-		287, 116,
-		294, 114,
-		298, 111,
-		294, 107
+	int Boton_Left[30] = {
+		286, 103,
+		279, 104,
+		275, 107,
+		272, 112,
+		272, 118,
+		277, 121,
+		283, 122,
+		289, 122,
+		295, 121,
+		300, 119,
+		304, 115,
+		303, 110,
+		300, 106,
+		295, 104,
+		290, 103
 	};
 
-	int Boton_Center[18] = {
-		323, 83,
-		316, 84,
-		312, 90,
-		316, 93,
-		323, 94,
-		329, 92,
-		332, 89,
-		331, 86,
-		327, 84
+	int Boton_Center[30] = {
+		315, 82,
+		310, 84,
+		306, 88,
+		306, 93,
+		310, 96,
+		315, 98,
+		321, 99,
+		326, 99,
+		331, 97,
+		335, 95,
+		338, 91,
+		335, 86,
+		331, 83,
+		325, 81,
+		320, 81
 	};
 
-	int Boton_Right[18] = {
-		356, 104,
-		351, 105,
-		346, 109,
-		346, 114,
-		350, 117,
-		358, 117,
-		364, 115,
-		365, 109,
-		361, 105
+
+	int Boton_Right[32] = {
+		353, 103,
+		348, 104,
+		342, 107,
+		339, 111,
+		340, 116,
+		343, 120,
+		348, 122,
+		354, 123,
+		360, 123,
+		365, 122,
+		370, 119,
+		373, 115,
+		372, 110,
+		368, 106,
+		363, 104,
+		358, 103
 	};
 
-	ricks.add(App->physics->CreateChain(0, 0, rick_head, 88, 0));
+	int Part_center_Left_Withrest[18] = {
+		130, 262,
+		157, 347,
+		157, 351,
+		156, 354,
+		136, 368,
+		130, 370,
+		97, 369,
+		128, 332,
+		128, 265
+	};
+	int Part_center_Right_Withrest[20] = {
+		501, 258,
+		467, 347,
+		466, 352,
+		469, 357,
+		489, 368,
+		495, 370,
+		527, 369,
+		508, 312,
+		503, 284,
+		503, 263
+	};
+
+	ricks.add(App->physics->CreateChain(0, 0, rick_head, 94, 0));
 	ricks.add(App->physics->CreateChain(0, 0, PartUP_right, 40, 0));
-	ricks.add(App->physics->CreateChain(0, 0, PartUP_center, 14, 0));
-	ricks.add(App->physics->CreateChain(0, 0, PartUP_center_2, 16, 0));
-	ricks.add(App->physics->CreateChain(0, 0, PartUp_Left, 42, 0));
-	ricks.add(App->physics->CreateChain(0, 0, Part_Center_1, 74, 0));
+	ricks.add(App->physics->CreateChain(0, 0, PartUP_center, 22, 0));
+	ricks.add(App->physics->CreateChain(0, 0, PartUP_center_2, 24, 0));
+	ricks.add(App->physics->CreateChain(0, 0, PartUp_Left, 56, 0));
+	ricks.add(App->physics->CreateChain(0, 0, Part_Center_1, 84, 0));
 	ricks.add(App->physics->CreateChain(0, 0, Part_Center_2, 22, 0));
-	ricks.add(App->physics->CreateChain(0, 0, Part_Down_Left, 14, 3));
-	ricks.add(App->physics->CreateChain(0, 0, Part_Right_Left, 12, 3));
+	ricks.add(App->physics->CreateChain(0, 0, Part_Down_Left, 14, 0));
+	ricks.add(App->physics->CreateChain(0, 0, Part_Right_Left, 12, 0));
 	ricks.add(App->physics->CreateChain(0, 0, Part_Left, 20, 0));
 	ricks.add(App->physics->CreateChain(0, 0, Part_Right, 20, 0));
-	ricks.add(App->physics->CreateChain(0, 0, Boton_Left, 16, 2));
-	ricks.add(App->physics->CreateChain(0, 0, Boton_Center, 18, 2));
-	ricks.add(App->physics->CreateChain(0, 0, Boton_Right, 18, 2));
+	ricks.add(App->physics->CreateChain(0, 0, Boton_Left, 30, 2));
+	ricks.add(App->physics->CreateChain(0, 0, Boton_Center, 30, 2));
+	ricks.add(App->physics->CreateChain(0, 0, Boton_Right, 32, 2));
+	ricks.add(App->physics->CreateChain(0, 0, Part_center_Left_Withrest, 18, 1));
+	ricks.add(App->physics->CreateChain(0, 0, Part_center_Right_Withrest, 20, 1));
+
 
 	return true;
 }
